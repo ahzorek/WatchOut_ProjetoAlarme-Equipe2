6cdea3878db8342b3a0066213b0c97c1d2b6ef51
Andre <andrezorek@gmail.com>
2023-12-20 19:56:42 -0300

    :sparkles: feat: 2.005: rotas user (get all, get, post config, delete)

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import AlarmController from "./app/controllers/AlarmController.js"
 import WeatherController from "./app/controllers/WeatherController.js"
 import DateTimeController from "./app/controllers/DateTimeController.js"
+import UserController from "./app/controllers/UserController.js"
 
 const router = Router()
 
@@ -36,5 +37,23 @@ router.put("/alarm/:id", AlarmController.update)
 
 //deleta alarme (recebe user id)
 router.delete("/alarm/:id", AlarmController.delete)
+
+
+//// user stuff
+
+//retorna lista de users
+router.get("/user", UserController.index)
+
+//retorna um user por id passada
+router.get("/user/:id", UserController.show)
+
+//cria novo user
+router.post("/user", UserController.store)
+
+//atualiza dados de user (recebe user id)
+// router.put("/user/:id", UserController.update)
+
+//deleta user (recebe user id)
+router.delete("/user/:id", UserController.delete)
 
 export default router
